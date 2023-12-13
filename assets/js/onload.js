b5ea09c40479c5586789ec49aa3780cb75805684
Shuhei Ohno <ohno.github@gmail.com>
2023-12-13 20:22:53 +0900

    Add tab support

--- assets/js/onload.js
+++ assets/js/onload.js
@@ -13,4 +13,21 @@ Start writing!
 `;
   }
   document.getElementById('output').value = math(document.getElementById('input').value);
+
+  // tab support
+  document.getElementById('input').addEventListener('keydown', function (e) {
+    var elem, end, start, value;
+    if (e.keyCode === 9) {
+      if (e.preventDefault) {
+        e.preventDefault();
+      }
+      elem = e.target;
+      start = elem.selectionStart;
+      end = elem.selectionEnd;
+      value = elem.value;
+      elem.value = "" + (value.substring(0, start)) + "\t" + (value.substring(end));
+      elem.selectionStart = elem.selectionEnd = start + 1;
+      return false;
+    }
+  });
 }
