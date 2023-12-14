ab3d9a1bc62cb7a6bc7ac31f918e3b8070affdd9
Shuhei Ohno <ohno.github@gmail.com>
2023-12-14 09:21:22 +0900

    Change textarea height

--- assets/js/math.js
+++ assets/js/math.js
@@ -1,6 +1,15 @@
 // https://qiita.com/kosuke_shimizu/items/187467354c32a7e18d07
 function math(input) {
+  // update URL
   url(input);
+  // update textarea size
+  var Hin  = document.getElementById('input').scrollHeight;
+  var Hout = document.getElementById('output').scrollHeight;
+  document.getElementById('input').height = (Math.max(Hin, Hout)*2) + "px";
+  console.log(document.getElementById('input').height);
+  console.log(document.getElementById('input').scrollHeight);
+
+  // update LaTeX
   document.getElementById('output').innerHTML = input.replace(/</g, "&lt;").replace(/>/g, "&gt;");
   // https://docs.mathjax.org/en/v3.2-latest/web/typeset.html#handling-asynchronous-typesetting
   MathJax.typesetPromise().then(() => {
